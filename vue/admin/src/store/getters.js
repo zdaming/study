b22a025821aec42fc8f5df3045eb049de42975bb
zdaming <zdaming123@gmail.com>
2020-04-16 17:29:54 +0800

    add admin-template

--- vue/admin/src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  routers: state => state.routerData.routers,
-  isCollapse: state => state.aside.isCollapse,
-  rightNav: state => state.tabNav.rightNav,
-  tabNavBox: state => state.tabNav.tabNavBox
-}
-
-export default getters
--- vue/admin/src/views/Page404.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="page404">
-    <p>PAGE NOT FOUND</p>
-  </div>
-</template>
